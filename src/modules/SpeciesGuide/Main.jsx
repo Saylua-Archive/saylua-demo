c8773f039bfe95485c6c804576ba79841e592e6c
Tiffany Zhang <zhangtiff@google.com>
2018-04-03 02:41:37 -0700

    Add back old pet profile page design.

--- src/modules/SpeciesGuide/Main.jsx
+++ src/modules/SpeciesGuide/Main.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import SayluaView from 'modules/SayluaView';
-
 import SpeciesList from './SpeciesList';
 import SpeciesView from './SpeciesView';
 import CoatView from './CoatView';
