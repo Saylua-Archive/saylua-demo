be5d67c63cc37eb2058e787fdb47e5a08d8c4f1a
Tiffany Zhang <zhangtiff@google.com>
2018-04-08 21:43:14 -0700

    Add back SayluaView

---
+++ src/components/SayluaView/Navbar/Main.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import { Link } from 'react-router-dom';
+
+import './Navbar.css';
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div id="navbar-container" className="navbar-container">
+        <div id="navbar" className="navbar">
+          <div id="navbar-inner-container" className="navbar-inner-container">
+            <div className="sidebar-filler" />
+            <div className="navbar-block">
+              <Link to="/" className="navbar-link" title="Home">
+                <i className="fas fa-fw fa-home" aria-hidden="true" />
+              </Link>
+            </div>
+            <div className="navbar-block">
+              <Link to="/den" className="navbar-link" title="Your Den">
+                Your Den
+              </Link>
+            </div>
+            <div className="navbar-block">
+              <Link to="/arcade/bloxi" className="navbar-link" title="Arcade">
+                Arcade
+              </Link>
+            </div>
+            <div className="navbar-block">
+              <Link to="/species" className="navbar-link" title="Species Guide">
+                Species Guide
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
