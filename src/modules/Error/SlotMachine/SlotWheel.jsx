720285e3a6b3b7a969235dcd9938b2eaf4a44e52
Tiffany Zhang <zhangtiff@google.com>
2018-05-11 18:45:20 -0700

    Add 404 slot machine.

---
+++ src/modules/Error/SlotMachine/SlotWheel.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import './SlotMachine.css';
+
+const WHEEL_HEIGHT = 300;
+
+export default class SlotWheel extends Component {
+  render() {
+    const topOffset = -WHEEL_HEIGHT * this.props.index;
+    return (
+      <div className="wheel">
+        <div className="wheel-inner-wrapper" style={{ transform: `translateY(${topOffset}px)` }}>
+          {
+            this.props.items.map(item => (
+              <div className="slot">
+                { item }
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    );
+  }
+}
