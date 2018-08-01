773b27bc3b793922cdcfdef5af7fd2789af17ab1
LikeMyBread <mbradley31@outlook.com>
2018-07-31 18:19:12 -0700

    Basic adventure render

--- src/modules/Adventure/AdventureScene.jsx
+++ src/modules/Adventure/AdventureScene.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
 export default function AdventureScene(props) {
+  const sceneTuples = [];
   const sceneItems = [];
   for (let i = 0; i < props.items.length; i++) {
+    sceneTuples.push([props.items[i], Math.random() * 100]);
+  }
+  sceneTuples.sort((a, b) => b[1] - a[1]);
+  for (let i = 0; i < sceneTuples.length; i++) {
     sceneItems.push(<AdventureSceneItem
-      src={props.items[i]}
+      src={sceneTuples[i][0]}
+      y={sceneTuples[i][1]}
+      key={sceneTuples[i][0]}
     />);
   }
+
   return (
     <div
       className="adventure-scene"
-      style={{ backgroundImage: props.background }}
+      style={{
+        backgroundImage: props.background,
+      }}
     >
       {sceneItems}
     </div>
@@ -18,14 +28,14 @@ export default function AdventureScene(props) {
 }
 
 function AdventureSceneItem(props) {
-  const SCALE = 25;
-  const HORIZON = 45;
-  const x = Math.random() * 100;
-  const y = Math.random() * HORIZON;
+  const SCALE = 33;
+  const HORIZON = 40;
+  const x = (Math.random() * 100) - (SCALE / 2);
+  const y = props.y;
   const z = Math.random() * 0;
-  const SCALEs = `${SCALE * (100 + (-0.0462 * y) + (0.0000102 * y * y))}%`;
+  const SCALEs = `${(SCALE / 100) * (100 + (-0.882 * y) + (0.00372 * y * y))}%`;
   const xs = `${x}%`;
-  const ys = `${y}%`;
+  const ys = `${y * (HORIZON / 100)}%`;
   return (
     <img
       alt=""
@@ -35,6 +45,7 @@ function AdventureSceneItem(props) {
         width: SCALEs,
         left: xs,
         bottom: ys,
+        filter: `saturate(${100 - ((y / 100) * 40)}%)`,
       }}
     />
   );
